a73e5ecdd6df943885f4baf434c4ac581d1e10c2
mace <mace666@hotmail.com>
2016-02-10 08:00:56 +0100

    Make sure following code is not executed if redirection is in progress.

--- src/index.php
+++ src/index.php
@@ -1,4 +1,6 @@
 <?php
-
 header('Location: Creator/version4/index.php');
 //header('Location: maintenence.html');
+/* Make sure following code is not executed if redirection is in progress */
+exit;
+?>
